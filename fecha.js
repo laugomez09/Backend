d64120ad6ae71dbfeae19abecda6732f54faeb18
Lautaro Thomas Gomez Muñoz <lautarothomasgomezmunoz@gmail.com>
2024-02-22 17:26:11 -0300

    Pusheando el primer desafio

--- fecha.js
+++ fecha.js
@@ -1,4 +1,3 @@
-
 const fecha = new Date();
 const dia = fecha.getDate().toString().padStart(2, "0");
 const mes = (fecha.getMonth() + 1).toString().padStart(2, "0");
@@ -8,19 +7,46 @@ const fechaFormatoDDMMYYYY = `${dia}/${mes}/${año}`;
 
 console.log(fechaFormatoDDMMYYYY); // Salida: 16/02/2024¨
 
-const fs = require("fs")
+const file = require("fs")
 
-let filePath = "./Hora_Dia_Mes_Ano"
+const path = "mi_path";
 
-let archivo = fs.writeFile(filePath, fechaFormatoDDMMYYYY (error => {
-    if (error) console.error("No se ha podido crear.")
-},
-fs.readFile(filePath,"utf8", (error, respuesta)=>{
-    if (error){
-        console.error("No se puede leer archivo")
+file.writeFile(`${path}/fecha.txt`, fechaFormatoDDMMYYYY, (err => {
+    if (err) console.error("No se ha podido crear el archivo")
+
+    file.readFile(path, "agregar", "utf-8", (error, respuesta) => {
+        if (error) {
+            console.error("No se puede leer archivo")
+            return
+        } else {
+            console.log(respuesta)
+        }
+    })
+}))
+/*
+const fecha = new Date();
+const dia = fecha.getDate().toString().padStart(2, "0");
+const mes = (fecha.getMonth() + 1).toString().padStart(2, "0");
+const año = fecha.getFullYear();
+
+const fechaFormatoDDMMYYYY = `${dia}/${mes}/${año}`;
+
+console.log(fechaFormatoDDMMYYYY); // Salida: 16/02/2024
+
+const path = "mi_path";
+
+const file = require("fs")
+
+file.writeFile(`${path}/fecha.txt`, fechaFormatoDDMMYYYY, (err) => {
+    if (err) {
+        console.error(err)
         return
-    }else{
-        console.log(respuesta)
     }
-})
-))
+    file.readFile(`${path}/fecha.txt`, "utf-8", (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        console.log(data)
+    })
+})*/
